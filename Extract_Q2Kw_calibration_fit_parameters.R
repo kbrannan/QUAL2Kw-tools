2d77db2ef2583c7bc35c8d5705604c4ec78f5334
dsobota <sobota.daniel@deq.state.or.us>
2018-02-16 15:08:08 -0800

    Update on extracting min and max pH data

--- Extract_Q2Kw_calibration_fit_parameters.R
+++ Extract_Q2Kw_calibration_fit_parameters.R
@@ -46,6 +46,10 @@ for (y in 1:length(Avg.grab.data)) {
 
 # Get names of columns
 Avg.grab.nm <- as.data.frame(strsplit(Q2Kw.output[Avg.grab.nm.loc], split = "  "))
+names(Avg.grab.nm) <- "Parameter.nm"
+Avg.grab.nm <- Avg.grab.nm[!apply(Avg.grab.nm == "", 1, all),]
+grab.nm <- paste(Avg.grab.nm)
+grab.nm <- sub(" ", "", grab.nm)
 
 # Get rid of blanks
 Avg.grab.nm <- Avg.grab.nm[!sapply(Avg.grab.nm, function(x) all(x == ""))]
@@ -63,4 +67,98 @@ for (z in 1:length(Q2Kw.list)) {
 Q2Kw.df <- Q2Kw.df[!sapply(Q2Kw.df, function(x) all(x == ""))]
 
 # Give columns the appropriate names
-names(Q2Kw.df) <- header.nm
+names(Q2Kw.df) <- grab.nm
+
+# Extract average grab sample data for assessment of fit----
+Grab.fit.pm.avg <- subset(Q2Kw.df, select = c("Reach", "Fast CBOD", "Organic N", "NH4-N", "NO3+NO2-N", "Organic P", "Inorganic P", "pH"))
+
+# Need to pull out Min pH----
+Min.grab.loc <- grep("Daily minimum water quality summary for main channel", Q2Kw.output)[1] #First occurence is the target line
+Min.grab.nm.loc <- Min.grab.loc + 1
+Min.grab.sites <- c(Min.grab.loc + 8, Min.grab.loc + 9, Min.grab.loc + 11) # Need to manually set based on site locations
+
+Min.grab.data <- Q2Kw.output[Min.grab.sites]
+
+Q2Kw.list <- list()
+
+# Seperate data on at least two spaces then create data frame using rbind
+for (y in 1:length(Min.grab.data)) {
+  Q2Kw.list[[y]] <- as.data.frame(strsplit(Min.grab.data[y], split = "  "))
+  names(Q2Kw.list[[y]]) <- y
+  Q2Kw.list[[y]] <- t(Q2Kw.list[[y]])
+}
+
+# Get names of columns
+Min.grab.nm <- as.data.frame(strsplit(Q2Kw.output[Min.grab.nm.loc], split = "  "))
+names(Min.grab.nm) <- "Parameter.nm"
+Min.grab.nm <- Min.grab.nm[!apply(Min.grab.nm == "", 1, all),]
+grab.nm <- paste(Min.grab.nm)
+grab.nm <- sub(" ", "", grab.nm)
+
+# Get rid of blanks
+Min.grab.nm <- Min.grab.nm[!sapply(Min.grab.nm, function(x) all(x == ""))]
+
+# Set blank data frame
+Q2Kw.df <- data.frame(stringsAsFactors = F)
+
+# Build dataframe with rbind fill so that empty cells don't repeat
+for (z in 1:length(Q2Kw.list)) {
+  Q2Kw.df <- bind_rows(Q2Kw.df, as.data.frame(Q2Kw.list[[z]], 
+                                              stringsAsFactors = F))
+}
+
+# Get rid of blanks
+Q2Kw.df <- Q2Kw.df[!sapply(Q2Kw.df, function(x) all(x == ""))]
+
+# Give columns the appropriate names
+names(Q2Kw.df) <- grab.nm
+
+# Extract Minimum grab sample data for assessment of fit----
+Grab.fit.pm.Min <- subset(Q2Kw.df, select = c("Reach", "pH"))
+names(Grab.fit.pm.Min) <- c("Reach", "Min pH")
+
+# Need to pull out Max pH----
+Max.grab.loc <- grep("Daily maximum water quality summary for main channel", Q2Kw.output)[1] #First occurence is the target line
+Max.grab.nm.loc <- Max.grab.loc + 1
+Max.grab.sites <- c(Max.grab.loc + 8, Max.grab.loc + 9, Max.grab.loc + 11) # Need to manually set based on site locations
+
+Max.grab.data <- Q2Kw.output[Max.grab.sites]
+
+Q2Kw.list <- list()
+
+# Seperate data on at least two spaces then create data frame using rbind
+for (y in 1:length(Max.grab.data)) {
+  Q2Kw.list[[y]] <- as.data.frame(strsplit(Max.grab.data[y], split = "  "))
+  names(Q2Kw.list[[y]]) <- y
+  Q2Kw.list[[y]] <- t(Q2Kw.list[[y]])
+}
+
+# Get names of columns
+Max.grab.nm <- as.data.frame(strsplit(Q2Kw.output[Max.grab.nm.loc], split = "  "))
+names(Max.grab.nm) <- "Parameter.nm"
+Max.grab.nm <- Max.grab.nm[!apply(Max.grab.nm == "", 1, all),]
+grab.nm <- paste(Max.grab.nm)
+grab.nm <- sub(" ", "", grab.nm)
+
+# Get rid of blanks
+Max.grab.nm <- Max.grab.nm[!sapply(Max.grab.nm, function(x) all(x == ""))]
+
+# Set blank data frame
+Q2Kw.df <- data.frame(stringsAsFactors = F)
+
+# Build dataframe with rbind fill so that empty cells don't repeat
+for (z in 1:length(Q2Kw.list)) {
+  Q2Kw.df <- bind_rows(Q2Kw.df, as.data.frame(Q2Kw.list[[z]], 
+                                              stringsAsFactors = F))
+}
+
+# Get rid of blanks
+Q2Kw.df <- Q2Kw.df[!sapply(Q2Kw.df, function(x) all(x == ""))]
+
+# Give columns the appropriate names
+names(Q2Kw.df) <- grab.nm
+
+# Extract Maximum grab sample data for assessment of fit----
+Grab.fit.pm.Max <- subset(Q2Kw.df, select = c("Reach", "pH"))
+names(Grab.fit.pm.Max) <- c("Reach", "Max pH")
+
